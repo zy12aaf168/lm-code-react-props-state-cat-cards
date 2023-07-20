66a7ff242d93c15742b946b9770f1c4b99d15a9e
Zulfu Yilmaz <z.yilmaz08@gmail.com>
2023-07-20 20:36:34 +0100

    Added dogdata in to the app. Changed the interfaces to be more accomodating for both cats and dogs and even more animals in the future

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -2,21 +2,21 @@ import cat1 from '../assets/CATIMAGES/cat1.jpg';
 import {CatImage} from './cat_image';
 import { CATIMAGES } from '../data/cat';
 
-interface CatCardProps {
+interface AnimalProps {
     id?: string;
     name: string;
     species: string;
     favFoods: Array<string>;
     birthYear: number;
-    catIndex: number;
+    animalIndex: number;
 }
 
-const CatCard: React.FC<CatCardProps> = ({id, name, species, favFoods, birthYear, catIndex}) => {
+const CatCard: React.FC<AnimalProps> = ({id, name, species, favFoods, birthYear, animalIndex: catIndex}) => {
     return (
     <div className="card">
         <h3 className="card__text card__header">{name}</h3>
         <p className="card__text">Species: {species}</p>
-        <p className="card__text">Favourite Food(s): {favFoods}</p>
+        <p className="card__text">Favourite Food(s): {favFoods.join(',')}</p>
         <p className="card__text">Birth Year: {birthYear}</p>
         <CatImage {...CATIMAGES[catIndex]} />
     </div>
